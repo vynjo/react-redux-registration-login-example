fa4784b596811849318deec55601587e3ae2a253
Christian Madsen <madsen.chris@gmail.com>
2022-03-26 17:35:14 -0400

    indenting correction

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,46 +10,46 @@ import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        history.listen((location, action) => {
-            // clear alert on location change
-            this.props.clearAlerts();
-        });
-    }
+    history.listen((location, action) => {
+      // clear alert on location change
+      this.props.clearAlerts();
+    });
+  }
 
-    render() {
-        const { alert } = this.props;
-        return (
-            <div className="jumbotron">
-                <div className="container">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        {alert.message &&
+  render() {
+    const { alert } = this.props;
+    return (
+      <div className="jumbotron">
+        <div className="container">
+          <div className="col-sm-8 col-sm-offset-2">
+            {alert.message &&
                             <div className={`alert ${alert.type}`}>{alert.message}</div>
-                        }
-                        <Router history={history}>
-                            <Switch>
-                                <PrivateRoute exact path="/" component={HomePage} />
-                                <Route path="/login" component={LoginPage} />
-                                <Route path="/register" component={RegisterPage} />
-                                <Redirect from="*" to="/" />
-                            </Switch>
-                        </Router>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+            }
+            <Router history={history}>
+              <Switch>
+                <PrivateRoute exact path="/" component={HomePage} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/register" component={RegisterPage} />
+                <Redirect from="*" to="/" />
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapState(state) {
-    const { alert } = state;
-    return { alert };
+  const { alert } = state;
+  return { alert };
 }
 
 const actionCreators = {
-    clearAlerts: alertActions.clear
+  clearAlerts: alertActions.clear
 };
 
 const connectedApp = connect(mapState, actionCreators)(App);
