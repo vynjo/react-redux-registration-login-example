0816d2f873e1214afce381f8669ae22510643f79
Christian Madsen <madsen.chris@gmail.com>
2022-03-26 19:12:39 -0400

    another eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-let HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
   plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html'
+    template: './src/index.html',
   })],
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   externals: {
     // global app config object
     config: JSON.stringify({
-      apiUrl: 'http://localhost:4000'
-    })
-  }
+      apiUrl: 'http://localhost:4000',
+    }),
+  },
 };
