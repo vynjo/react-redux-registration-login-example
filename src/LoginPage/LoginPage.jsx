2d0ba35e57f3a5c3990e41ab24ec6b1dd5a765ed
Christian Madsen <madsen.chris@gmail.com>
2022-03-26 19:14:14 -0400

    clean up comments

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,70 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-
+import { connect, useDispatch } from 'react-redux';
 import { userActions } from '../_actions';
 
 // https://www.digitalocean.com/community/tutorials/five-ways-to-convert-react-class-components-to-functional-components-with-react-hooks
 // https://olinations.medium.com/10-steps-to-convert-a-react-class-component-to-a-functional-component-with-hooks-ab198e0fa139
 
 function LoginPage(props) {
-
   const { loggingIn } = props;
-//   const [details, setDetails] = useState({username: "", password: ""})
+  //   const [details, setDetails] = useState({username: "", password: ""})
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [submitted, setSubmitted] = useState(false);
-  
-//   const handleUserNameInput = e => {
-//     console.log('here', e);
-//   };
-//   const handlePasswordInput = e => {
-//     console.log('here', e);
-//   };
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {
-      e
-       console.log('here');
-
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setSubmitted(true);
+    if (username && password) {
+      dispatch(userActions.login(username, password));
+    }
   };
-  
-  //   useEffect(()=>{
-  //     setUsername('');
-  //     setPassword('');
-  //   },[]);
-  useEffect(()=>{
-    console.log('component mounted!');
-  },[]);
+
   return (
     <div className="col-md-6 col-md-offset-3">
       <h2>Login</h2>
-      <form name="form" onSubmit={handleSubmit(username, password)}>
-        <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+      <form name="form" onSubmit={handleSubmit}>
+        <div className={`form-group${submitted && !username ? ' has-error' : ''}`}>
           <label htmlFor="username">Username</label>
-          <input type="text" className="form-control" name="username" value={username} onChange={e => setUsername({username: e.target.value})} />
-          {submitted && !username &&
-                            <div className="help-block">Username is required</div>
-          }
+          <input type="text" className="form-control" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          {submitted && !username
+                            && <div className="help-block">Username is required</div>}
         </div>
-        <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+        <div className={`form-group${submitted && !password ? ' has-error' : ''}`}>
           <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" name="password" value={password} onChange={e => setUsername({password: e.target.value})} />
-          {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-          }
+          <input type="password" className="form-control" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          {submitted && !password
+                            && <div className="help-block">Password is required</div>}
         </div>
         <div className="form-group">
           <button className="btn btn-primary">Login</button>
-          {loggingIn &&
-            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-          }
+          {loggingIn
+            && <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />}
           <Link to="/register" className="btn btn-link">Register</Link>
         </div>
       </form>
     </div>
   );
 }
-
 
 function mapState(state) {
   const { loggingIn } = state.authentication;
@@ -73,7 +56,7 @@ function mapState(state) {
 
 const actionCreators = {
   login: userActions.login,
-  logout: userActions.logout
+  logout: userActions.logout,
 };
 
 const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
