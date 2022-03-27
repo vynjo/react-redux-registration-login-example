ef4001351410a137cd16f3b978bfe865c3da8bf1
Christian Madsen <madsen.chris@gmail.com>
2022-03-26 21:01:36 -0400

    remove error

--- src/_reducers/alert.reducer.js
+++ src/_reducers/alert.reducer.js
@@ -1,6 +1,6 @@
 import { alertConstants } from '../_constants';
 
-export default function alert(state = {}, action) {
+export function alert(state = {}, action) {
   switch (action.type) {
     case alertConstants.SUCCESS:
       return {
